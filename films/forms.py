6c9d05d4ea53ed459946b3133f2e83d5608f009e
dridanasiba <nasiba.mabetova11@gmail.com>
2025-05-28 20:44:49 +0600

    test2

--- films/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from . import models
-
-class FilmsForm(forms.ModelForm):
-    class Meta:
-        model = models.Films
-        fields = "__all__"
--- parser_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.Parser_Rezka)
-
